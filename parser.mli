94a4360217b696455832caa9d24fc50cbb7c866c
srweiner <srw2163@columbia.edu>
2014-11-09 15:13:29 -0500

    added tokens to parser.mly
    14 rules never reduced error. tokens need to be added in grammar.

--- parser.mli
+++ parser.mli
@@ -4,37 +4,54 @@ type token =
   | SEMI
   | RPAREN
   | RBRACE
+  | MOD
   | COMMA
+  | PERIOD
   | EOF
   | PLUS
   | TIMES
+  | LINK
+  | BILINK
   | MINUS
   | DIVIDE
-  | ASSIGN
-  | PERIOD
   | EQ
+  | ASSIGN
   | NEQ
   | LEQ
   | GEQ
   | LT
   | GT
-  | OR
-  | AND
   | NOT
+  | AND
+  | OR
   | IF
   | ELSE
   | WHILE
   | FOR
   | RETURN
   | GRAPH
+  | NODETYPE
+  | EDGETYPE
   | NODE
   | BOOL
   | STRING
   | STRUCT
   | PRINT
   | NEW
-  | ID
+  | CONTINUE
+  | DOUBLE
+  | FALSE
+  | TRUE
+  | INT
+  | VOID
+  | DEST
+  | EDGES
+  | STATIC
+  | CHR
+  | DO
+  | IN
   | NUM of (int)
+  | ID of (string)
 
 val program :
   (Lexing.lexbuf  -> token) -> Lexing.lexbuf -> unit
