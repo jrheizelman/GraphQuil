cff37262f6792c61af1e36da8a5176019bb88901
Jon Paul <jpaul@dyn-160-39-236-182.dyn.columbia.edu>
2014-12-11 22:03:26 -0500

    edits to getting string names

--- intermediate.ml
+++ intermediate.ml
@@ -1,48 +1,42 @@
-open Ast
-open Sast
-open Semantic_check
+open ast
+open sast
+open semantic_check
 
 (* Returns the string name for the date type*)
-let rec get_data_type = function
+let rec get_datatype_name = function
  
-Int_Literal_t (n, d) -> string_of_int n
- | Double_Literal_t of double * validtype list (*before we include this, we need to define string_of_double in ast*)
- | Bool_Literal_t (l, d) -> string_of_bool l
- | Char_Literal_t (n, d) -> "\'" ^ (String.make 1) n ^"\'"
- | Noexpr_t -> "" (*don't see Noexpr in our ast*)
- | Id_t (s, d) -> s  (*will double check this construction vs. (match (String.get s 0) with 'R' -> s^"()"*)
- | Binop_t of expr_t (e1, op, e2, d) -> get_data_type e1 ^
+Literal_t (t) -> string_of_int t
+ | Id_t (_, t, _) -> t 
+ | Binop_t (d, e1, op, e2) -> get_datatype_name e1 ^
  	(match op with
-	  Add -> "+ " ^ get_data_type e2 
-	  | Sub -> "- " ^ get_data_type e2 
-	  | Mult -> "* " ^ get_data_type e2
-	  | Div -> "/ " ^ get_data_type e2
-	  | Mod -> "mop" ^ get_data_type e2
-	  | Child -> "%" ^ get_data_type e2
-	  | Equal -> "== " ^ get_data_type e2 
-	  | Neq -> "!= " ^ get_data_type e2
-      | Less -> "< " ^ get_data_type e2 
-      | Leq -> "<= " ^ get_data_type e2 
-      | Greater -> "> " ^ get_data_type e2 
-	  | Geq -> ">= " ^ get_data_type e2
-	  | And -> "&& " ^ get_data_type e2
-	  | Or -> "|| " ^ get_data_type e2 
+	  Add -> " + " ^ get_datatype_name e2 
+	  | Sub -> " - " ^ get_datatype_name e2 
+	  | Mult -> " * " ^ get_datatype_name e2
+	  | Div -> " / " ^ get_datatype_name e2
+	  | Mod -> " mod " ^ get_datatype_name e2
+	  | Child -> " % " ^ get_datatype_name e2
+	  | Equal -> " == " ^ get_datatype_name e2 
+	  | Neq -> " != " ^ get_datatype_name e2
+      | Less -> " < " ^ get_datatype_name e2 
+      | Leq -> " <= " ^ get_datatype_name e2 
+      | Greater -> " > " ^ get_datatype_name e2 
+	  | Geq -> " >= " ^ get_datatype_name e2
+	  | And -> " && " ^ get_datatype_name e2
+	  | Or -> " || " ^ get_datatype_name e2 )
+ | Unop_t (d, e1, op) -> get_datatype_name e1 ^
+ 	(match op with
+ 	 Neg-> "-"
+ 	 | Not -> "!")
+ | Call_t (f, ) (*unsure of meaning of construction in semantic file*)
+ | Array_t (t, _, _) -> t (*should be similar to Assign_t with get_datatype_name function implemented *)
+ | String_Lit_t (s) -> s
+ | Char_t (c) -> c (*does this not have to be "\'" ^ (String.make 1) n ^"\'"?*)
+ | Assign_t (t, l, _) -> t ^ " = " ^ get_datatype_name l (* doesn't this needs to be redined in semantic file as Assign_t (t, _, _)->?*)
+ | Construct_t (t, _) -> t  (*should be similar to Assign_t with get_datatype_name function implemented *)
+ | MakeArr_t (t, _) -> t
+ | Access_t (t, _, _) -> t
+ | Bool_Literal_t (b) -> string_of_bool b
+ | Noexpr_t -> "" (*don't see Noexpr in our ast*)
 
- | Call_t (f, argl, d) (*this will be lengthy*)
 
- (*below this line is still in progress*)
- | Array_t of expr_t * expr_t * validtype list
- | Not_t of expr_t * validtype list
- | String_t of string * validtype list
- | Assign_t of expr * expr * validtype list
- | Construct_t of validtype * expr list * validtype list
- | MakeArr_t of validtype * expr * validtype list
- | Access_t of string * string * validtype list
-
-
-
-type ir_expr=
-Ir_Int_Literal of ir
-
-type ir_stmt=
-|Ir_If of ir_var_decl * string 
+(* Returns the datatype declaration in java *)
