1bd0394a630ebb30b3d2090ce71df81929c1a6cb
Jon Paul <jpaul@dyn-160-39-236-183.dyn.columbia.edu>
2014-12-13 03:48:49 -0500

    java decs

--- intermediate.ml
+++ intermediate.ml
@@ -1,8 +1,12 @@
+(* 
+Written by Jon Paul
+*)
+
 open ast
 open sast
 open semantic_check
 
-(* Returns the string name for the date type*)
+(* Returns the name for the date type as a string*)
 let rec get_datatype_name = function
  
 Literal_t (t) -> string_of_int t
@@ -28,18 +32,18 @@ Literal_t (t) -> string_of_int t
  	 Neg-> "-"
  	 | Not -> "!")
  | Call_t (f, ) (*unsure of meaning of construction in semantic file*)
- | Array_t (t, _, _) -> t (*should be similar to Assign_t with get_datatype_name function implemented *)
+ (*| Array_t (t, _, _) -> t Removed from SAST, no longer needed atm*) (*should be similar to Assign_t with get_datatype_name function implemented *)
  | String_Lit_t (s) -> s
  | Char_t (c) -> c (*does this not have to be "\'" ^ (String.make 1) c ^"\'"?*)
- | Assign_t (t, l, _) -> get_datatype_name t ^ " = " ^ get_datatype_name l (* doesn't this needs to be redined in semantic file as Assign_t (t, _, _)->?*)
- (*| Construct_t (t, _) *)-> t  (*should be similar to Assign_t with get_datatype_name function implemented *)
- | MakeArr_t (t, _) -> t
+ | Assign_t (t, l, _) -> t ^ " = " ^ l (* doesn't this needs to be redined in semantic file as Assign_t (t, _, _)->?*)
+ (*| Construct_t (t, _) -> Removed from SAST, no longer needed atm*)
+ (*| MakeArr_t (t, _) -> t Removed from SAST, no longer neeeded atm*)
  | Access_t (t, _, _) -> t
  | Bool_Literal_t (b) -> string_of_bool b
  | Noexpr_t -> "" (*don't see Noexpr in our ast*)
 
 
-(* Returns the java form of a statement*)
+(* Returns the statement in java form as a string*)
 let rec get_java_statement = function
 
 Block_t (stmts) -> "{\n" ^ String.concat "" (List.map get_java_statement stmts) ^ "}\n" (*maps the recursive function to each element in the statement list provided as a function argument*)
@@ -59,7 +63,22 @@ Block_t (stmts) -> "{\n" ^ String.concat "" (List.map get_java_statement stmts) 
 | While_t (expr, stmts) -> "while (" ^ get_datatype_name expr ^ ") " ^ get_java_statement stmts
 
 
-
+(* Returns java declaration of the datatype as a string*)
+(* Not entire sure of left hand side construction *)
+and get_java_declaration hasID expr =
+	(match hasID with 
+		(*each of these is taken from the ast.ml file with the line "type validtype"*)
+		validtype(Int)-> "int "
+		| validtype(Char)-> "char "
+		| validtype(String)-> "String "
+		| validtype(Bool)-> "boolean "
+		| validtype(Void)-> "void " ) ^ " " ^
+ 		(match expr with
+			Assign_t (t, l, _)->  t ^ 
+			(match hasID with 
+				validtype(String)-> "= new String(" ^ get_datatype_name expr ^ ");"
+				| _ -> "= " ^ get_datatype_name expr ^ ";")
+		| _ -> get_datatype_name expr ^ ";")
 
 
 (*in let rec get_datatype_as_string hasName = function *)
