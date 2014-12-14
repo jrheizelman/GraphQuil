4e66411d3dc25c9d663ff2f68200b546deba601f
Steven Weiner <srw2163@columbia.edu>
2014-12-14 15:52:13 -0500

    fixed bugs in produceJava.ml so will compile
    new error compiling graphQuil.ml

--- produceJava.ml
+++ produceJava.ml
@@ -11,8 +11,6 @@ open Semantic_check
 
 (* Returns the string name for the date type*)
 let rec get_datatype_name = function
- 
-
 Literal_t (t) -> string_of_int t
  | Id_t (_, t, _) -> t 
  | Binop_t (datatype, e1, op, e2) -> get_datatype_name e1 ^
@@ -46,31 +44,23 @@ Literal_t (t) -> string_of_int t
  | Bool_Lit_t (b) -> string_of_bool b
  | Noexpr_t -> "" (*don't see Noexpr in our ast*)
 
-
-(* Returns the statement in java form as a string*)
+ (* Returns the statement in java form as a string*)
 let rec get_java_statement = function
 
 Block_t(t) -> "{\n" ^ String.concat "" (List.map get_java_statement t.statements_t) ^ "}\n" (*maps the recursive function to each element in the statement list provided as a function argument*)
-
 | Expr_t (expr) -> get_datatype_name expr^ ";\n";
-
 | Return_t (expr) -> "return " ^ get_datatype_name expr ^ ";\n"; 
-
 (*| If_t (expr, stmts, Block_t([]))-> "if (" ^ get_datatype_name expr ^ ")\n" ^ List.map get_java_statement stmts *)
-
 | If_t (expr, stmts, stmts2) -> 
 		(match stmts2.statements_t with 
 		 [] -> "if (" ^ get_datatype_name expr ^ ")\n" ^ Sast.string_of_block_t stmts
 		| _ -> "if (" ^ get_datatype_name expr ^ ")\n" ^ Sast.string_of_block_t stmts ^ "else\n" ^ Sast.string_of_block_t stmts2)
-
 | For_t (expr1, expr2, expr3, stmts) -> "for ( int " ^ get_datatype_name expr1 ^ "=" ^ get_datatype_name expr2 ^ "; i<=" ^ get_datatype_name expr3 ^ ";" ^get_datatype_name expr1 ^ "++)" ^ Sast.string_of_block_t stmts
-
 | While_t (expr, stmts) -> "while (" ^ get_datatype_name expr ^ ") " ^ Sast.string_of_block_t stmts
-
 
 (* Returns java declaration of the datatype as a string*)
 (* Not entirely sure that left hand side is valid in this form *)
-and get_java_declaration hasID expr =
+let get_java_declaration hasID expr = 
 	(match hasID with 
 		(*each of these is taken from the ast.ml file with the line "type validtype"*)
 		(*if Int doesn't work, revert back to validtype(int)*)
@@ -81,36 +71,27 @@ and get_java_declaration hasID expr =
 		| Void -> "void " ) ^ " " ^
  		(match expr with
 			Assign_t (t, expr1, _) -> get_datatype_name expr1 ^ 
-			(match hasID with 
-				String-> "= new String(" ^ get_datatype_name expr ^ ");"
-				| _ -> "= " ^ get_datatype_name expr ^ ";")
-		| _ -> get_datatype_name expr ^ ";")
+				(match hasID with 
+					String-> "= new String(" ^ get_datatype_name expr ^ ");"
+					| _ -> "= " ^ get_datatype_name expr ^ ";")
+			| _ -> get_datatype_name expr ^ ";")
 
+let rec writeToFile fileName pString = 
+  let file = open_out (fileName ^ ".java") in
+    fprintf file "%s"  pString
 
-	and createJavaProgram program=
-	let body= String.concat "" (List.rev(List.map get_java_statement program))
-	in 
-	sprintf 
-	"
-
-	public class Graphquil
-
-	{
-
-		public static void main(String[] args)
-		{
-
+and createJavaProgram program = 
+	let body = String.concat "" (List.rev (List.map get_java_statement program)) in
+	let output = sprintf "
+	public class GraphQuil {
 		%s
-
-		}
-
 	}
+	" body in
+	writeToFile "graphQuil" output
 
-	" body
-
-let _ =
+(*let _ =
 	let lexbuf = Lexing.from_channel stdin in
 	let program = Parser.program Scanner.token lexbuf in
 	let file = open_out "graphquil.java" in
-	let _ = createJavaProgram file program
-	close_out file
+	createJavaProgram file program
+	close_out file*)
